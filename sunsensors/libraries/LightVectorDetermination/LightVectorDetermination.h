c19a08be62fa9adfc2b62e8a2223dc49e9d0474f
Matt Wright <16mpw1@queensu.ca>
2021-11-13 14:43:46 -0500

    Added sunsensor interface code

---
+++ sunsensors/libraries/LightVectorDetermination/LightVectorDetermination.h
@@ -0,0 +1,24 @@
+#ifndef LightVectorDetermination_h
+#define LightVectorDetermination_h
+
+#include "Arduino.h"
+#include "SqrtFit.h"
+
+class LightVectorDetermination {
+  public:
+    SqrtFit* light_models;
+    int n_photodiode;
+    int n_readings;
+    int photo_pin_offset;
+    int n_avg;
+
+    LightVectorDetermination(int n_photodiode, int n_readings, int pin_offset, int n_avg);
+    void fit(double** voltages);
+    float get_global_angle();
+    float get_global_angle(double * voltages);
+    void read_photodiode_array(double * voltages);
+    void read_photodiode_array(double ** voltages, int col);
+    float _read_photodiode(int pin);
+};
+
+#endif
